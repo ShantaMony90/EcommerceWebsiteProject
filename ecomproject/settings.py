24d9330f5d70422686adf7837226d6af7ba0f5ba
ShantaMony90 <shantamony90@gmail.com>
2024-10-04 17:41:38 +0600

    first commit

--- ecomproject/settings.py
+++ ecomproject/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'store',
+    'user_app',
 ]
 
 MIDDLEWARE = [
@@ -64,6 +65,9 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'store.context_processors.g_category',
+                'store.context_processors.g_cart',
+                'store.context_processors.g_wishlist',
             ],
         },
     },
